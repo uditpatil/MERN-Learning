29733588b0ec88ea1186677b506e861c5ebbef1d
uditpatil <uditnp08@gmail.com>
2022-11-05 16:00:20 -0700

    first assignment javascript

---
+++ Javascript learning/Patil_Udit_predict_the_output.js
@@ -0,0 +1,85 @@
+//Problem 1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// preducted output: 
+//Tesla
+//Mercedes
+
+
+
+//Problem 2
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+//predicted output:
+//reference error name is not defined
+//if we comment "console.log(name);" output will be Elon
+
+
+//problem 3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//predicted output:
+//12345
+//Undefined since hashedPassword is not an object inside person object 
+
+
+//Problem 4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//predicted output:
+//false
+//true
+
+
+//problem 5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//predicted output:
+//value
+//[1,5,1,8,3,3]
+//1
+//5
