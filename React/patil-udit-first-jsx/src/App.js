1ae24dccbe7886433322c304ed3f66c62e5c1cd6
uditpatil <uditnp08@gmail.com>
2022-11-06 17:19:28 -0800

    first jsx

---
+++ React/patil-udit-first-jsx/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <li>Learn React</li>
+      <li>Climb Et. Mountain</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </div>
+  );
+}
+
+export default App;
